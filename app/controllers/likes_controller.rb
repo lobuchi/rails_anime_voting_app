8be84bc0c8e9f547092e6320b159b829fb0f711c
Lohit <lohituchil@gmail.com>
2025-10-08 22:50:31 +0100

    upvote and dislike

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,26 @@
 class LikesController < ApplicationController
 
   def create
-    @anime = Anime.Find(params[:anime_id])
-    @like = @anime.likes.build(user: :current_user)
-    @like.save
+    @anime = Anime.find(params[:anime_id])
+    @like = Like.new(anime: @anime,user: current_user)
+    if @like.save
+      redirect_to @anime, notice: "Upvoted!"
+    else
+      redirect_to @anime, alert: "You already liked this."
+    end
   end
 
   def destroy
+        @anime = Anime.find(params[:anime_id])
     @like = current_user.likes.find_by(anime: @anime)
+    if @like
     @like.destroy
+    notice = "Removed your vote."
+    else
+    notice = "You haven't liked this anime yet."
+    end
+
+       redirect_to @anime, notice: notice
   end
 
 end
